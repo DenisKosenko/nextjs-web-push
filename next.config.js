6676d954a2c0a38a5852831d45962ba3a13e1fb7
Kosenko, Denys (contracted) <Denys.Kosenko@contracted.pmi.com>
2024-11-12 12:54:06 +0200

    Update cors

--- next.config.js
+++ next.config.js
@@ -3,36 +3,15 @@ const nextConfig = {
     async headers() {
         return [
             {
+                // matching all API routes
                 source: '/api/:path*',
                 headers: [
                     { key: 'Access-Control-Allow-Credentials', value: 'true' },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: 'http://localhost:3000',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: 'https://localhost:3000',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: 'https://cit-mercury-web.iinc-dev.aws.pmicloud.biz',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: 'https://cit-mercury-web.iinc-qa.aws.pmicloud.biz',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: 'https://preprod.cit-mercury-web.iinc-qa.aws.pmicloud.biz',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS',
-                    },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
                     {
                         key: 'Access-Control-Allow-Headers',
-                        value: 'Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
                     },
                 ],
             },
